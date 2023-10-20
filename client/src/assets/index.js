6a61933d6ade8d92e96fddaae8fcee9b0f64f430
Harsh Khavale <harshkhavale1102@gmail.com>
2023-10-20 16:55:26 +0530

    Delete client directory

--- client/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import bharat from './bharat.png';
-import logo from './logo.jpg';
-import g20 from './g20.png';
-import banner1 from './banner1.jpg';
-import banner2 from './banner2.jpg';
-import policebg from './policebg.jpeg'
-export {bharat,logo,g20,banner1,banner2,policebg};
